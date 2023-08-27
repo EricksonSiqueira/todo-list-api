61ef80798bad01d627494c192123fd53a2c94e0d
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-27 14:35:01 -0300

    feat: create todo controllers

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import { todoService } from '../services';
+
+export const todoController = {
+  async create(req: Request, res: Response) {
+    const { body } = req;
+
+    const todo = await todoService.create(body);
+
+    if (todo.error) {
+      return res.status(todo.status).json({ error: { message: todo.error } });
+    }
+
+    return res.status(todo.status).json(todo.data);
+  },
+
+  async findAll(_req: Request, res: Response) {
+    const todos = await todoService.findAll();
+
+    return res.status(todos.status).json(todos.data);
+  },
+
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const todo = await todoService.findById(+id);
+
+    res.status(todo.status).json(todo.data);
+  },
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { body } = req;
+
+    const updatedTodo = await todoService.update(+id, body);
+
+    if (updatedTodo.error) {
+      return res
+        .status(updatedTodo.status)
+        .json({ error: { message: updatedTodo.error } });
+    }
+
+    return res
+      .status(updatedTodo.status)
+      .json({ message: updatedTodo.message });
+  },
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const deleteTodo = await todoService.delete(+id);
+
+    if (deleteTodo.error) {
+      return res
+        .status(deleteTodo.status)
+        .json({ error: { message: deleteTodo.error } });
+    }
+
+    return res.status(deleteTodo.status).json();
+  },
+};
