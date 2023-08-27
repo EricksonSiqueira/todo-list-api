e2ccbb260add5fa82b65f1ec2131378512d5f0e2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-27 14:28:10 -0300

    feat: adjust sql initial script

--- db/script.sql
+++ db/script.sql
@@ -3,17 +3,17 @@ CREATE DATABASE todolistdb;
 
 USE todolistdb;
 
-DROP TABLE IF EXISTS todo;
+DROP TABLE IF EXISTS todos;
 
-CREATE TABLE IF NOT EXISTS todo (
+CREATE TABLE IF NOT EXISTS todos (
     id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    todo_description VARCHAR(255) NOT NULL,
-    done TINYINT(1) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    done TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
-INSERT INTO todo (title, todo_description, done) VALUES 
+INSERT INTO todos (title, description, done) VALUES 
     ('title1', 'description1', 0),
     ('title2', 'description2', 1),
     ('title3', 'description3', 1);
