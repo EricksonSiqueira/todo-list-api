78c3160eb6fe0c160efcbbeb24ee8c9aa21c217b
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-02 04:26:48 -0300

    test: create allTodosResponse mock variable

---
+++ tests/integration/mocks/todo.integration.mock.ts
@@ -0,0 +1,7 @@
+import { Todo } from '../../../src/types/todo';
+
+export const allTodosResponse = [
+  { id: 1, title: 'title1', description: 'description1', done: 0 },
+  { id: 2, title: 'title2', description: 'description2', done: 0 },
+  { id: 3, title: 'title3', description: 'description3', done: 0 },
+] as Todo[];
