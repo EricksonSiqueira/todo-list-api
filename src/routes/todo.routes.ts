da92f023f60be38bc90806141c62471b1a7a22a1
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-02 04:08:33 -0300

    feat: use validate body middleware in routes that use body

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -4,10 +4,20 @@ import { todoMiddleware } from '../middlewares/todo.middleware';
 
 const todoRoute = Router();
 
-todoRoute.post('/', todoMiddleware.validateNewTodo, todoController.create);
+todoRoute.post(
+  '/',
+  todoMiddleware.validateBody,
+  todoMiddleware.validateNewTodo,
+  todoController.create
+);
 todoRoute.get('/', todoController.findAll);
 todoRoute.get('/:id', todoMiddleware.validateId, todoController.findById);
-todoRoute.put('/:id', todoMiddleware.validateId, todoController.update);
+todoRoute.put(
+  '/:id',
+  todoMiddleware.validateId,
+  todoMiddleware.validateBody,
+  todoController.update
+);
 todoRoute.delete('/:id', todoMiddleware.validateId, todoController.delete);
 
 export { todoRoute };
