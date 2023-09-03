aa3753e71b1bc9b7c2117db37a13c5d626558176
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-03 18:18:50 -0300

    feat: add security

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
 import { todoRoute } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(compression());
+app.use(helmet());
 
 app.use('/todos', todoRoute);
 
